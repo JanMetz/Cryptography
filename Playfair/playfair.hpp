adb5487e24423a04bf4c4676023d3c847e06cc20
Jan Metzler <metzler.infa@gmail.com>
2023-03-06 14:45:28 +0100

    code cleanup

--- Playfair/playfair.hpp
+++ Playfair/playfair.hpp
@@ -20,6 +20,7 @@ private:
 	std::vector<std::array<char, 2>> chopMessage(const std::string& msg) const;
 	std::string getCodedLetters(const std::pair<int, int> &in1, const std::pair<int,int> &in2, const int addVal) const;
 	std::pair<int,int> getIndexes(const char &letter) const;
+	static bool checkForIllegalChars(const std::string &text);
 
 	std::string mKey;
 	const std::array<char, 25> mAlphabet{ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
